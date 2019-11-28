94dddc574bcc28c8864f9463941059a0e9724dea
moayada <moayada@kth.se>
2019-11-28 07:42:00 +0100

    Added the Home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './components/home-view/home-view.component';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
+import { DateAndTimeComponent } from './components/date-and-time/date-and-time.component';
+import { ConcatenatePipe } from './components/current-weather/concatenate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    CurrentWeatherComponent,
+    DateAndTimeComponent,
+    ConcatenatePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
